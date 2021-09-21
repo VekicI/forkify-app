e874e97c69610e16a9df7d744fd3eaab8277a474
VekicI <ivekic@inforcetechnologies.com>
2021-09-21 14:54:02 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3774ea16-1040-4431-b149-d1130c206635';
+export const MODAL_CLOSE_SECONDS = 2.5;
